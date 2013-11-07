d485381d5d378ccfd6aefb345b9c0e61b326d4ab
Agustin Calderon <agustincl@gmail.com>
2013-11-07 21:24:18 +0100

    Grupos

--- public/index.php
+++ public/index.php
@@ -8,6 +8,8 @@ define('APPLICATION_ENV',
 
 require_once ("../application/model/generalModel.php");
 
+require_once ("../application/autoload.php");
+
 $config_file="../application/configs/config.ini";
 $config=readConfigFile($config_file, APPLICATION_ENV);
 
@@ -17,10 +19,15 @@ switch ($request['controller'])
 {
 	case 'users':
 		include_once("../application/controllers/usersController.php");
-		break;
+	break;
+	case 'groups':
+		//include_once("../application/controllers/groupsController.php");
+		$controller = new Controllers_Groups($request);		
+		$controller->indexAction(array());
+	break;
 	case 'index':
 		include_once("../application/controllers/indexController.php");
-		break;
+	break;
 	default:
 		break;
 }
