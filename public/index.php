15944baa8dbb9c3b1464f0f3d30056cf0c621d7a
Agustin Calderon <agustincl@gmail.com>
2013-11-11 19:27:56 +0100

    Bootstrap

--- public/index.php
+++ public/index.php
@@ -7,24 +7,13 @@ define('APPLICATION_ENV',
 				getenv('APPLICATION_ENV') : 'production'));
 
 $config_file="../application/configs/config.ini";
-
 require_once ("../application/autoload.php");
 
-require_once ("../application/model/generalModel.php");
+$boostrap = new Boostrap($config);
+$boostrap->run();
 
 
 
-
-$config=readConfigFile($config_file, APPLICATION_ENV);
-
-$request=getRequest();
-
-
-	/** New controller logic */
-	$controllerName = "Controllers_" . ucfirst($request['controller']); 
-	$controller = new $controllerName($request);
-	$methodName=strtolower($request['action']) . "Action";
-	$controller->$methodName(array());
 
 
 
