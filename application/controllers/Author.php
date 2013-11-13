727722b6608ae7af199a982975c2d8d125bbd159
Tamara <tamaracive@yahoo.es>
2013-11-13 19:56:27 +0100

    Gmail

--- application/controllers/Author.php
+++ application/controllers/Author.php
@@ -25,6 +25,46 @@ class Controllers_Author
 	{
 		$this->content=renderView($this->request,$viewparams);
 	}
+	
+	public function logingmailAction($viewparams)
+	{
+		$config = array(
+				'callbackUrl' => 'http://anguilaperro.org/author/gmailCallback',
+				'siteUrl'      => 'https://accounts.google.com/o/oauth2/auth',							
+				'consumerKey' => '78200168710.apps.googleusercontent.com',
+				'consumerSecret' => 'Sa2FTOg6cmtGkfiqba8MUf44'
+		);
+		$consumer = new Zend_Oauth_Consumer($config);
+		
+		$token = $consumer->getRequestToken();		
+		$_SESSION['GMAIL_REQUEST_TOKEN'] = serialize($token);
+		$consumer->redirect();
+	}
+	
+	public function gmailCallbackAction($viewparams)
+	{
+		$config = array(
+				'callbackUrl' => 'http://anguilaperro.org/author/gmailCallback',
+				'siteUrl'      => 'https://accounts.google.com/o/oauth2/auth',							
+				'consumerKey' => '78200168710.apps.googleusercontent.com',
+				'consumerSecret' => 'Sa2FTOg6cmtGkfiqba8MUf44'
+		);
+		$consumer = new Zend_Oauth_Consumer($config);
+		 
+		if (!empty($_GET) && isset($_SESSION['GMAIL_REQUEST_TOKEN'])) {
+		    $token = $consumer->getAccessToken(
+		                 $_GET,
+		                 unserialize($_SESSION['GMAIL_REQUEST_TOKEN'])
+		             );
+		    $_SESSION['GMAIL_REQUEST_TOKEN'] = serialize($token);
+		 
+		    // Now that we have an Access Token, we can discard the Request Token
+		    $_SESSION['GMAIL_REQUEST_TOKEN'] = null;
+		} else {
+		    // Mistaken request? Some malfeasant trying something?
+		    echo ('Invalid callback request. Oops. Sorry.');
+		}
+	}
 
 	public function __destruct()
 	{
